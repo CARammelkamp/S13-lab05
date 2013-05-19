d52d53b084747377aad1898dffe763a4d6952d72
Christina Morris <cmmorris@umail.ucsb.edu>
2013-05-19 15:28:42 -0700

    Added advanced class with Diamond and Diamond Ring

---
+++ src/edu/ucsb/cs56/S13/drawings/cmmorris/advanced/AllMyDrawings.java
@@ -0,0 +1,162 @@
+package edu.ucsb.cs56.S13.drawings.cmmorris.advanced;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;  // single lines
+import java.awt.geom.Ellipse2D;  // ellipses and circles
+import java.awt.geom.Rectangle2D; // for the bounding box
+import java.awt.Rectangle;  // squares and rectangles
+import java.awt.geom.GeneralPath; // combinations of lines and curves
+import java.awt.geom.AffineTransform; // translation, rotation, scale
+import java.awt.Shape; // general class for shapes
+import java.awt.Color; // class for Colors
+import java.awt.Stroke;
+import java.awt.BasicStroke;
+
+
+import edu.ucsb.cs56.S13.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.S13.drawings.utilities.GeneralPathWrapper;
+
+/**
+ * A class with static methods for drawing various pictures
+ * @author Christina Morris
+ * @author Phill Conrad 
+ * @version for CS56, lab06, Spring 2013
+ */
+
+
+public class AllMyDrawings
+{
+    /** Draw a picture with a few Diamonds and Diamond Rings
+     */
+
+    public static void drawPicture1(Graphics2D g2) {
+
+	Diamond d1 = new Diamond(100,250,50,50);
+
+	g2.setColor(Color.CYAN); g2.draw(d1);
+	
+	// Make a black diamond that's half the size, 
+	// and moved over 150 pixels in x direction
+
+	Shape d2 = ShapeTransforms.scaledCopyOfLL(d1,0.5,0.5);
+	d2 = ShapeTransforms.translatedCopyOf(d2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(d2);
+	
+	// Here's a diamond that's 4x as big (2x the original)
+	// and moved over 150 more pixels to right.
+	d2 = ShapeTransforms.scaledCopyOfLL(d2,4,4);
+	d2 = ShapeTransforms.translatedCopyOf(d2,150,0);
+	
+	// We'll draw this with a thicker stroke
+	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
+	
+	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
+	// #002FA7 is "International Klein Blue" according to Wikipedia
+	// In HTML we use #, but in Java (and C/C++) its 0x
+	
+	Stroke orig=g2.getStroke();
+	g2.setStroke(thick);
+	g2.setColor(new Color(0x002FA7)); 
+	g2.draw(d2); 
+	
+	// Draw two diamond rings
+	
+	DiamondRing dr1 = new DiamondRing(50,200,150, 150);
+	DiamondRing dr2 = new DiamondRing(200,300,150,150);
+	
+	g2.draw(dr1);
+	g2.setColor(new Color(0x8F00FF)); g2.draw(dr2);
+	
+	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
+	
+	g2.setStroke(orig);
+	g2.setColor(Color.BLACK); 
+	g2.drawString("A few Diamonds by Christina Morris", 20,20);
+    }
+
+
+    /** Draw a picture with a few diamonds
+     */
+    public static void drawPicture2(Graphics2D g2) {
+
+	// Draw some diamonds.
+	
+	Diamond large = new Diamond(100,50,200,200);
+	Diamond smallD = new Diamond(20,50,40,40);
+	Diamond tallSkinny = new Diamond(20,150,20,40);
+	Diamond shortFat = new Diamond(20,250,40,20);
+	
+	g2.setColor(Color.RED);     g2.draw(large);
+	g2.setColor(Color.GREEN);   g2.draw(smallD);
+	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
+	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
+	
+	Diamond d1 = new Diamond(100,250,50,50);
+	g2.setColor(Color.CYAN); g2.draw(d1);
+	
+	// Make a black diamondring that's half the size, 
+	// and moved over 150 pixels in x direction
+	Shape d2 = ShapeTransforms.scaledCopyOfLL(d1,0.5,0.5);
+	d2 = ShapeTransforms.translatedCopyOf(d2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(d2);
+	
+	// Here's a diamond that's 4x as big (2x the original)
+	// and moved over 150 more pixels to right.
+	d2 = ShapeTransforms.scaledCopyOfLL(d2,4,4);
+	d2 = ShapeTransforms.translatedCopyOf(d2,150,0);
+	
+	// We'll draw this with a thicker stroke
+	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
+	
+	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
+	// #002FA7 is "International Klein Blue" according to Wikipedia
+	// In HTML we use #, but in Java (and C/C++) its 0x
+	
+	Stroke orig=g2.getStroke();
+	g2.setStroke(thick);
+	g2.setColor(new Color(0x002FA7)); 
+	g2.draw(d2); 
+	
+	// Draw two Diamond Rings
+	
+        DiamondRing dr1 = new DiamondRing(50,350,50,50);
+	DiamondRing dr2 = new DiamondRing(200,350,100,100);
+	
+	g2.draw(dr1);
+	g2.setColor(new Color(0x8F00FF)); 
+
+	// Rotate the second ring 45 degrees around its center.
+	Shape dr3 = ShapeTransforms.rotatedCopyOf(dr2, Math.PI/4.0);
+
+	g2.draw(dr3);
+	
+	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
+	
+	g2.setStroke(orig);
+	g2.setColor(Color.BLACK); 
+	g2.drawString("A bunch of Diamonds and Diamond Rings by Christina", 20,20);
+    }
+  
+    /** Draw a different picture with a few diamonds.
+     */
+
+    public static void drawPicture3(Graphics2D g2) {
+	
+	// label the drawing
+	
+	g2.drawString("A bunch of Diamonds by Christina Morris", 20,20);
+
+	
+	// Draw some Diamonds.
+	
+	Diamond large = new Diamond(100, 100, 300, 300);
+	Diamond small = new Diamond(20, 50, 30, 30);
+       
+       g2.setColor(Color.RED);     g2.draw(large);
+       g2.setColor(Color.GREEN);   g2.draw(small);
+       
+       
+    }
+    
+
+}
